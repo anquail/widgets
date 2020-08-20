3a00656d677be8fd8473addfce3c3e63708b5f0d
anquail <63658161+anquail@users.noreply.github.com>
2020-08-20 18:19:41 -0400

    updated dropdown and search

--- src/components/ColorText.js
+++ src/components/ColorText.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const ColorText = ({ selected }) => {
 	return (
-		<div style={{ color: `${selected.label}` }}>This text is {selected.label}!</div>
+		<div className="ui container" style={{ color: `${selected.label}`, textAlign: 'center', marginTop: '30px' }}>
+			<h1>This text is {selected.label}!</h1>
+		</div>
 	);
 };
 
